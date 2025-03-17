e875389b3c826ff54c2aa7be798af6ebe899405a
Bruno <brunosouzaleal@hotmail.com>
2025-03-17 18:10:45 +0000

    att

--- Python/CL 18/ex088.py
+++ Python/CL 18/ex088.py
@@ -1,7 +1,33 @@
-# Create a program to help a MEGA SENA player generate guesses.  
+# Create a program to help a lottery player generate guesses.  
 # The program will ask how many games should be generated  
 # and will randomly select 6 numbers between 1 and 60 for each game,  
 # storing everything in a nested list.  
 
-from random import randint
+import random
+
+lottery_number = []
+
+n = int(input('How many games would you like to do: '))
+c = 0
+m = 0
+
+# is the counter to generate the correcta amount of games, once the counter reaches the users input it will stop generating 
+while c != n:
+    c+=1
+    print(f'The {c} game is: ',end='')
+    # Generate a random number
+    for g in range (0,6):
+        m = random.randint(1, 60)
+        # Check if the generate number is already in the list if it is, it will create a new number
+        if m in lottery_number:
+            while m in lottery_number:
+                m = random.randint(1, 60)
+        lottery_number.append(m)
+        # Generate the line with 6 numbers and clean the lottery list to make a new one in the other line 
+        if g == 5:
+            print(f'{lottery_number}')
+            lottery_number.clear()
+
+        
+
 
--- Python/CL 18/ex089.py
+++ Python/CL 18/ex089.py
@@ -2,3 +2,48 @@
 # and stores everything in a nested list.  
 # At the end, display a report showing each student's average grade  
 # and allow the user to view individual grades for each student.  
+
+main_list = []
+each_student = []
+loop_breaker = ''
+c = 0
+
+while True:
+    each_student.clear()
+    loop_breaker = ''
+# Gives the studends their own number
+    c += 1
+# Collect the students data
+    each_student.append(c-1)
+    each_student.append(str(input('Type the student full name here: ')))
+    each_student.append(int(input('Type the fist grade: ')))
+    each_student.append(int(input('Type the second grade: ')))
+    main_list.append(each_student[:])
+    
+# ask if the data inputer wants to keep adding data, if the answer is not Y or N, loop keeps going  
+    while loop_breaker not in ['Y','N']:
+        loop_breaker = (str(input(' Would you like to continue ? [Y] continue [N] stop: '))).upper().split()[0]
+        if loop_breaker not in ['Y','N']:
+            print('\033[1;31mTry Again !\033[m', end = '')
+
+    if loop_breaker == 'N':
+        break
+
+print('=-='*15)
+print(' '*12,'STUDENTS GRADE')
+print('=-='*15)
+print('N°      NAME                  AVG GRADE')
+print('=-='*15)
+
+for x in range(0, c):
+    avg = (main_list[x][2] + main_list[x][3]) / 2
+    print(f'{main_list[x][0]:<6}{main_list[x][1]:<22}{avg:>8.1f}')
+
+grade_details = 0
+
+while True:
+    grade_details = int(input('Type the student number to see the info detailed [999 interrompe]: '))
+    if grade_details == 999:
+        break
+    if grade_details >= 0:
+        print(f'{main_list[grade_details]}')
